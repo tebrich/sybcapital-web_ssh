0d25005c8734198bd8b31676af6b98e31c274327
Arias-xss <kokiarias01@gmail.com>
2023-02-27 14:27:18 -0300

    Nuxt changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,31 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} **/
 module.exports = {
   content: [
     './components/**/*.{js,vue,ts}',
     './layouts/**/*.vue',
     './pages/**/*.vue',
     './plugins/**/*.{js,ts}',
-    './nuxt.config.{js,ts}',
+    './nuxt.config.{js,ts}'
   ],
   prefix: 'sb-',
   theme: {
     container: { center: true },
     fontFamily: {
-      sans: ['Poppins', 'sans-serif'],
+      sans: ['Poppins', 'sans-serif']
     },
     screens: {
       sm: '600px',
       md: '960px',
       lg: '1264px',
-      xl: '1264px',
+      xl: '1264px'
     },
     extend: {
       colors: {
         primary: '#3E4095',
         secondary: '#ED3237',
-        font: '#606060',
-      },
-    },
+        font: '#606060'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
